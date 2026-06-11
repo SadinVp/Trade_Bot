4f9c9d5417d1f0b4661293daf58a3ac3794c286e
Mohammed Sadin vp <msadinvp@gmail.com>
2026-06-11 21:15:24 +0530

    removed temporary testing code

--- test_logger.py
+++
@@ -1,6 +0,0 @@
-from bot.logging_config import logger
-
-logger.info("Trading bot started successfully.")
-logger.error("This is a sample error message.")
-
-print("Log written successfully!")
--- test_order.py
+++
@@ -1,9 +0,0 @@
-from bot.orders import place_market_order
-
-response = place_market_order(
-    symbol="BTCUSDT",
-    side="BUY",
-    quantity=0.001,
-)
-
-print(response)
